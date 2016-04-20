13181c181a03f698c80c40efa79a27e248c3ca97
tanyang <122074809@qq.com>
2016-04-20 13:08:59 +0800

    save

--- TYPagerControllerDemo/ViewController.m
+++ TYPagerControllerDemo/ViewController.m
@@ -7,9 +7,9 @@
 //
 
 #import "ViewController.h"
-#import "CustomPagerController.h"
+#import "TYPagerController.h"
 
-@interface ViewController ()
+@interface ViewController ()<TYPagerControllerDataSource>
 
 @end
 
@@ -23,10 +23,29 @@
 
 - (void)addPagerController
 {
-    CustomPagerController *VC = [[CustomPagerController alloc]init];
+    TYPagerController *VC = [[TYPagerController alloc]init];
+    VC.dataSource = self;
     [self addChildViewController:VC];
     VC.view.frame = self.view.bounds;
     [self.view addSubview:VC.view];
+}
+
+- (NSInteger)numberOfControllersInPagerController
+{
+    return 30;
+}
+
+- (UIViewController *)pagerController:(TYPagerController *)pagerController controllerForIndex:(CGFloat)index
+{
+    UIViewController *VC = [[UIViewController alloc]init];
+    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 200, 50)];
+    label.center = VC.view.center;
+    label.text = [@(index) stringValue];
+    label.font = [UIFont systemFontOfSize:32];
+    label.textAlignment = NSTextAlignmentCenter;
+    [VC.view addSubview:label];
+    VC.view.backgroundColor = [UIColor colorWithRed:arc4random()%255/255.0 green:arc4random()%255/255.0 blue:arc4random()%255/255.0 alpha:arc4random()%255/255.0];
+    return VC;
 }
 
 - (void)didReceiveMemoryWarning {
