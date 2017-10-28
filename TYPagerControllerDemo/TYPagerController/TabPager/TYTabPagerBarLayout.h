678e192da744033c8cc85844bc285ce54d833308
tanyang <122074809@qq.com>
2017-10-28 09:36:39 +0800

    code reviewed

--- TYPagerControllerDemo/TYPagerController/TabPager/TYTabPagerBarLayout.h
+++ TYPagerControllerDemo/TYPagerController/TabPager/TYTabPagerBarLayout.h
@@ -32,7 +32,7 @@ typedef NS_ENUM(NSUInteger, TYPagerBarStyle) {
 
 // progress view
 @property (nonatomic, assign) CGFloat progressHeight;   // default 2
-@property (nonatomic, assign) CGFloat progressWidth; //if>0 progress width is equal,else progress width is cell width
+@property (nonatomic, assign) CGFloat progressWidth; //if > 0 progress width is equal,else progress width is cell width
 @property (nonatomic, strong, nullable) UIColor *progressColor;
 
 @property (nonatomic, assign) CGFloat progressRadius;   // height/2
@@ -43,7 +43,7 @@ typedef NS_ENUM(NSUInteger, TYPagerBarStyle) {
 @property (nonatomic, assign) CGFloat progressVerEdging; // default 0, cover style is 3.
 
 // cell frame
-@property (nonatomic, assign) CGFloat cellWidth; // default 0, if>0 cells width is equal,else if=0 cell will caculate all titles width
+@property (nonatomic, assign) CGFloat cellWidth; // default 0, if > 0 cells width is equal,else if = 0 cell will call delegate
 @property (nonatomic, assign) CGFloat cellSpacing; // default 2,cell space
 @property (nonatomic, assign) CGFloat cellEdging;  // default 0,cell left right edge
 @property (nonatomic, assign) BOOL adjustContentCellsCenter;// default NO, cells center if contentSize < bar's width ,will set sectionInset
@@ -53,7 +53,6 @@ typedef NS_ENUM(NSUInteger, TYPagerBarStyle) {
 @property (nonatomic, strong) UIFont *selectedTextFont;     // default 17
 @property (nonatomic, strong) UIColor *normalTextColor;     // default 51.51.51
 @property (nonatomic, strong) UIColor *selectedTextColor;   // default white
-
 @property (nonatomic, assign) BOOL textColorProgressEnable; // default YES
 
 //   animate duration
@@ -71,7 +70,6 @@ typedef NS_ENUM(NSUInteger, TYPagerBarStyle) {
 - (void)layoutSubViews;
 
 - (void)adjustContentCellsCenterInBar;
-
 
 // override
 - (void)transitionFromCell:(UICollectionViewCell<TYTabPagerBarCellProtocol> *_Nullable)fromCell toCell:(UICollectionViewCell<TYTabPagerBarCellProtocol> *_Nullable)toCell animate:(BOOL)animate;
